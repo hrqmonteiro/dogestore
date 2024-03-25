58f843256f689b7c3008b1afadcf80e2bed37ebc
hrqmonteiro <hrqmonteiro@protonmail.com>
2024-03-25 17:52:32 -0300

    feat: add router

---
+++ src/components/screens/cart.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function CartScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Carrinho!</Text>
+    </View>
+  )
+}
